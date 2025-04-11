4622b503dbbf1be3b2de5bc30a31a7a571e61cba
bentran1vn <tan182205@gmail.com>
2025-04-11 11:09:22 +0700

    FixGetListMessages

--- BEAUTIFY_SIGNALING.SERVICES/Services/ChatServices/ResponseModel.cs
+++ BEAUTIFY_SIGNALING.SERVICES/Services/ChatServices/ResponseModel.cs
@@ -5,4 +5,15 @@ public class ResponseModel
     public record ConversationResponseModel(
         Guid ConversationId, Guid EntityId ,string? FriendName, string? FriendImageUrl
     );
+    
+    public record MessageResponseModel(
+        Guid Id,
+        Guid ConversationId,
+        Guid SenderId,
+        bool IsClinic,
+        string SenderName,
+        string SenderImageUrl,
+        string Content,
+        DateTimeOffset CreatedOnUtc
+    );
 }
